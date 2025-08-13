5c1b5a708c52e1508518c1570ca49cb4231a0ba4
IchiroFukuda <ichr0113@gmail.com>
2025-08-13 13:52:57 +0900

    fix: color

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -133,7 +133,7 @@ export default function AuthPage() {
                 id="email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
-                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-black"
                 placeholder="your@email.com"
                 disabled={isLoading}
                 required
